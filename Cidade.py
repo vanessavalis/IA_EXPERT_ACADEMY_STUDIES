aa32dc3bc9a57aa5d146c2cc59b119384ab5a955
Vanessa <vanessa.lima_santos@outlook.com>
2021-09-26 20:11:46 -0300

    Criação da classe Cidade

---
+++ Cidade.py
@@ -0,0 +1,12 @@
+class Cidade:
+    def __init__(self, nome):
+        self.nome = nome
+        self.visitado = False
+        self.adjacentes = []
+        
+    def addCidadeAdjacente(self, cidade):
+        self.adjacentes.append(cidade)
+        
+'''c = Cidade ("Teste")
+c.nome
+c.visitado'''
